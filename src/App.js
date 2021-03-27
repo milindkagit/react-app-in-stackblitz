7964b37d28139a76412f7325f7e1324abaa5437f
Milind <milindkkulkarni@gmail.com>
2021-03-27 11:02:07 +0530

    minor updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+     
+
+      <h1>Hello ReactJS!</h1>
+      <p>This is so easy to create the HtML5 pages :)</p>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
